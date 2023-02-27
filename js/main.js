a4b589ebe1db09cbf7d508d1de13b5632bb636d7
tpett20 <thomaspettersen20@gmail.com>
2023-02-27 17:52:13 -0500

    corrected board adjustment in init function, began event listener handleClick function

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 /*----- constants -----*/
 
-// const colors = {
-//     '0': 'lightgrey',
-//     '1': 'lightblue',
-//     '-1': 'lightgreen'
-// }
+const colors = {
+    '0': 'lightgrey',
+    '1': 'lightblue',
+    '-1': 'lightgreen'
+}
 
-// const boardTest = {
-//     a: [1, 2, 3],
-//     b: [1, 2, 3],
-//     c: [1, 2, 3]
-// }
+const boardTest = {
+    a: [1, 2, 3],
+    b: [1, 2, 3],
+    c: [1, 2, 3]
+}
 
 /*----- state variables -----*/
 
@@ -30,18 +30,28 @@ function init() {
     for (square of squareEls) {
         square.classList.add('inactive')
     }
-    for (square of board){
-        square = 0
+    for (let i=0; i<9; i++){
+        board.push(0)
     }
+    console.log('Board of Zeroes', board)
+    turn = 0
     render()
 }
 
 function render() {
     console.log('rendering game')
+    if (turn % 2 === 0) {
+        squareChanger ++
+    } else {
+        squareChanger --
+    }
 }
 
 function setSquare(evt) {
-    
+    const squarePlayedIndex = evt.target.id
+    console.log('Square Index', squarePlayedIndex)
+    const squarePlayed = board[squarePlayedIndex]
+    console.log('Square Played Value', squarePlayed)
 }
 
 function checkWinStatus() {
@@ -63,6 +73,19 @@ function checkWinStatus() {
 }
 
 /*----- event listeners -----*/
+
+function handleClick(evt) {
+    console.log('this works')
+    setSquare(evt)
+    checkWinStatus()
+    turn++
+    render()
+}
+
+for (squareEl of squareEls) {
+    console.log(squareEl)
+    squareEl.addEventListener('click', handleClick)
+}
 
 // squareEls.addEventListener('click', setSquare)
 
